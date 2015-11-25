f825d9de205520e6fcfb63e5cfa930606da7c53b
masterUNG <phrombutr@gmail.com>
2015-11-25 14:59:27 +0700

    Check Zero

--- app/src/main/java/appewtc/masterung/easylearning/MainActivity.java
+++ app/src/main/java/appewtc/masterung/easylearning/MainActivity.java
@@ -2,20 +2,53 @@ package appewtc.masterung.easylearning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private ManageTABLE objManageTABLE;
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Bind Widget
+        bindWidget();
+
         //Connected Database
         objManageTABLE = new ManageTABLE(this);
 
     }   // onCreate
+
+    private void bindWidget() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+    }
+
+    public void clickLogin(View view) {
+
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        if (userString.equals("") || passwordString.equals("")) {
+
+            //Have Space
+            MyAlertDialog objMyAlertDialog = new MyAlertDialog();
+            objMyAlertDialog.myDialog(MainActivity.this, "มีช่องว่าง นะคะ", "กรุณากรอก ให้ครบทุกช่อง คะ");
+
+        } else {
+
+            //No Space
+
+        }
+
+    }   // clickLogin
+
+
 
 }   // Main Class
